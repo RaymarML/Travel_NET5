297ccf0723d97a0a779c5364550f5e26c4eed2aa
RaymarML <raymarmanresa@gmail.com>
2024-11-11 22:36:48 +0100

    Change tab size.

--- src/core/Travel.Application/Common/Behaviors/LoggingBehavior.cs
+++ src/core/Travel.Application/Common/Behaviors/LoggingBehavior.cs
@@ -5,19 +5,19 @@ using Microsoft.Extensions.Logging;
 
 namespace Travel.Application.Common.Behaviors
 {
-    public class LoggingBehavior<TRequest> : IRequestPreProcessor<TRequest>
-    {   
-        private readonly ILogger _logger;
+  public class LoggingBehavior<TRequest> : IRequestPreProcessor<TRequest>
+  {
+    private readonly ILogger _logger;
 
-        public LoggingBehavior(ILogger<TRequest> logger)
-        {
-            _logger = logger;
-        }
-
-        public async Task Process(TRequest request, CancellationToken cancellationToken)
-        {
-            var requestName = typeof(TRequest).Name;
-            _logger.LogInformation("Travel Request: {@Request}", requestName, request);
-        }
+    public LoggingBehavior(ILogger<TRequest> logger)
+    {
+      _logger = logger;
     }
+
+    public async Task Process(TRequest request, CancellationToken cancellationToken)
+    {
+      var requestName = typeof(TRequest).Name;
+      _logger.LogInformation("Travel Request: {@Request}", requestName, request);
+    }
+  }
 }
--- src/core/Travel.Application/Common/Behaviors/PerformanceBehavior.cs
+++ src/core/Travel.Application/Common/Behaviors/PerformanceBehavior.cs
@@ -6,31 +6,31 @@ using Microsoft.Extensions.Logging;
 
 namespace Travel.Application.Common
 {
-    public class PerformanceBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+  public class PerformanceBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+  {
+    private readonly Stopwatch _timer;
+    private readonly ILogger<TRequest> _logger;
+
+    public PerformanceBehavior(
+        ILogger<TRequest> logger)
     {
-        private readonly Stopwatch _timer;
-        private readonly ILogger<TRequest> _logger;
-
-        public PerformanceBehavior(
-            ILogger<TRequest> logger)
-        {
-            _timer = new Stopwatch();
-            _logger = logger;
-        }
-
-        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
-        {
-            _timer.Start();
-            var response = await next();
-            _timer.Stop();
-
-            var elapsedMilliseconds = _timer.ElapsedMilliseconds;
-            if (elapsedMilliseconds <= 500) return response;
-
-            var requestName = typeof(TRequest).Name;
-            _logger.LogWarning("Travel Long Running Request: {Name} ({ElapsedMilliseconds} milliseconds) {@Request}",
-                requestName, elapsedMilliseconds, request);
-            return response;
-        }
+      _timer = new Stopwatch();
+      _logger = logger;
     }
+
+    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    {
+      _timer.Start();
+      var response = await next();
+      _timer.Stop();
+
+      var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+      if (elapsedMilliseconds <= 500) return response;
+
+      var requestName = typeof(TRequest).Name;
+      _logger.LogWarning("Travel Long Running Request: {Name} ({ElapsedMilliseconds} milliseconds) {@Request}",
+          requestName, elapsedMilliseconds, request);
+      return response;
+    }
+  }
 }
